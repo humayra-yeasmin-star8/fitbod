9ab0404b8b6bed4f8e961eecc5fd5de78d0154da
humayra-yeasmin-star8 <humayrayeasmin15@gmai.com>
2026-09-24 12:46:04 +0600

    types added

---
+++ src/types.tsx
@@ -0,0 +1,12 @@
+
+export type Workout = {
+  id: string; 
+  name: string;
+  category: string[]; 
+  equipment: string; 
+  duration: number;
+  calories: number; 
+  rating: number; 
+  image: string; 
+
+};
